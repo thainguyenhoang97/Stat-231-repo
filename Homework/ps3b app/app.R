0746e248f2bd63b28d0af94c9353864622f4511e
Thai Nguyen <thainguyen@Thais-MacBook-Pro.local>
2020-09-17 23:50:27 +0700

    Created button, conditional panel

--- Homework/ps3b app/app.R
+++ Homework/ps3b app/app.R
@@ -2,33 +2,63 @@ library(fivethirtyeight)
 library(shinythemes)
 library(tidyverse)
 
+
 data("mad_men")
 summary(mad_men)
+
+  
 show_choice <- as.list(mad_men$show)
+perf <- as.list(mad_men$performer)
+
+#namecheck <- mad_men %>%
+ # filter(str_detect(performer, "\\") )
+
+#mad_men_new <- mad_men %>%
+ # mutate(name_new = stringi::stri_enc_toutf8(performer))
 
 ui <- fluidPage(
   h1("Actors of your favorite TV shows"),
   #Create drop down list for choosing shows
-  selectInput(inputId = "show_choice"
+  selectInput(inputId = "x"
               , label = "Choose a show of interest"
               , choices = show_choice
-              , selected = "Game of Thrones"
               ),
-  #Create action button to confirm interested show
-  actionButton(inputId = "button"
-               , label = "Ready"),
   
+  h4("Press confirm when you are sure with this choice of show"),
   
-  tableOutput(outputId = "table")
+  actionButton(inputId = "button", label = "Confirm"),
+  
+  conditionalPanel(condition = "input.button > 0"
+                   , selectInput(inputId = "y"
+                                 , label = "Choose a performer of interest"
+                                 , choices = perf_choice)
+  ),
+  
+  tableOutput(outputId = "table1"),
+  tableOutput(outputId = "table2")
 )
 
 server <- function(input, output) {
-  use_data <- reactive({
-    data <- filter(mad_men, show == input$show_choice)
+  use_data1 <- reactive({
+    data <- filter(mad_men, show == input$x)
   })
   
-  output$table <- renderTable({
-    dplyr::select(use_data(), input$show_choice, performer, show_start, show_end)
+  
+  use_data2 <- reactive({
+    data <- filter(mad_men2, performer == input$y)
+  })
+  
+  mad_men2 <- mad_men %>%
+    filter(show == input$x)
+  
+  perf_choice <- as.list(mad_men2$performer)
+  
+  output$table1 <- renderTable({
+    dplyr::select(use_data1(), performer, show_start, show_end)
+  })
+  
+  output$table2 <- renderTable({
+    dplyr::select(use_data2(), performer, num_lead, num_support, num_shows)
   })
   
 }
