b48576ede24e374c3d925810ee4c71c9b54fa590
Thai Nguyen <thainguyen@Thais-MacBook-Pro.local>
2020-09-18 22:59:04 +0700

    For submission

--- Homework/ps3b app/app.R
+++ Homework/ps3b app/app.R
@@ -2,24 +2,7 @@ library(fivethirtyeight)
 library(shinythemes)
 library(tidyverse)
 
-#namecheck <- fivethirtyeight::mad_men %>%
-#  mutate(performer2 = case_when(performer == "Alexander Skarsg\u008crd" ~ "Alexander Skarsgaard"
-#                                , str_detect(performer, "Freddy Rodr") ~ "WOO HOO!"
-#                                , TRUE ~ performer)
-#         , performer3 = str_replace(performer, "[:xdigit:]", "WOOHOO")
-#         , performer4 = str_replace(performer, "\\\\", "WOOHOO")
-#         , performer5 = str_replace(performer, "[:digit:]", "WOOHOO")
-#         , performer6 = stringi::stri_enc_toutf8(performer)
-#         ) %>%
-#  select(starts_with("performer"))
 
-# not ideal: but can hard code using str_detect
-# checking to make sure no others will be erroneously recorded 
-# i.e., that there is only one match per str_detect
-#length(which(str_detect(mad_men$performer, "Freddy Rodr")==TRUE))
-#length(which(str_detect(mad_men$performer, "Laura Cer")==TRUE))
-#length(which(str_detect(mad_men$performer, "Sevigny")==TRUE))
-#length(which(str_detect(mad_men$performer, "Alexander Skarsg")==TRUE))
 
 # fix wierd encodings
 mad_men2 <- fivethirtyeight::mad_men %>%
@@ -33,9 +16,6 @@ mad_men2 <- fivethirtyeight::mad_men %>%
 
 # Make actors/actresses status uppercase
 mad_men2$status = tolower(mad_men2$status)
-mad_men2 %>%
-  mutate(verb_status = case_when(mad_men2$status == "end" ~ "ended",
-                                 mad_men2$status == "left" ~ "left"))
 
 
 # these can be vectors
@@ -47,7 +27,7 @@ ui <- fluidPage(
   h1("How are the actors/actresses of your favorite TV shows doing?"),
   sidebarLayout(
     sidebarPanel(
-  #Create drop down list for choosing shows
+ 
       selectInput(inputId = "x"
                , label = "Choose a show of interest"
                , choices = show_choice
@@ -56,7 +36,8 @@ ui <- fluidPage(
       conditionalPanel(condition = "input.x != 0"
                        , selectInput(inputId = "y"
                                      , label = "Choose a performer of interest"
-                                     , choices = perf_choice)
+                                     , choices = perf_choice
+                                     , selected = NULL)
       ),
       
       h4("Click confirm if you want to learn about this actor/actress"),
@@ -65,11 +46,18 @@ ui <- fluidPage(
     ),
     
   mainPanel(
-  tableOutput(outputId = "table1"),
-  conditionalPanel(condition = "input.button > 0"
-                   , textOutput(outputId = "text")) 
-           )
-   )
+    fluidRow(
+      splitLayout(cellWidths = c("20%", "80%")
+                  , tableOutput("table1")
+                  , conditionalPanel(condition = "input.button > 0"
+                                     , textOutput(outputId = "text")
+                                     , textOutput(outputId = "text1")
+                                     )
+      ),
+    ),
+    fluidRow()
+    )
+  )
 )
 
 server <- function(input, output, session) {
@@ -100,9 +88,15 @@ server <- function(input, output, session) {
   output$text <- renderText({
     paste("Since", input$y, use_data2()$status, "his/her role in", 
           input$x, use_data2()$years_since, "years ago, he/she has appeared as a lead in",
-          use_data2()$num_lead, "show(s), as support in", use_data2()$num_support,
-          "show(s), and played an integral part in", use_data2()$num_shows, "show(s).")
+          use_data2()$num_lead, "show(s),")
   })
+  
+  output$text1 <- renderText({
+    paste("as support in", use_data2()$num_support,
+          "show(s), and played an integral part in", use_data2()$num_shows, "show(s).")
+  }
+    
+  )
 }
 
 shinyApp(ui = ui, server = server)
